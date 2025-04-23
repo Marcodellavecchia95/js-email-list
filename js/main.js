8269372e06d90d79abe9ee2b7a47a462cd52c3e1
Marcodellavecchia95 <mdellavecchia95@gmail.com>
2025-04-23 13:40:18 +0200

    add 10 mail random list

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const apiUri = `https://flynn.boolean.careers/exercises/api/random/mail`;
-const randomListEl = document.getElementById("container");
+const randomListEl = document.getElementById("list-group");
 
 // for (let i = 0; i <= 9; i++) {
 //   axios.get(apiUri).then((response) => {
@@ -7,12 +7,15 @@ const randomListEl = document.getElementById("container");
 //   });
 // }
 
-const generateRandomMails = () => {
-  for (let i = 0; i <= 9; i++) {
-    axios.get(apiUri).then((response) => {
-      console.log(response.data.response);
-    });
-  }
-};
-
-generateRandomMails();
+// const generateRandomMails = () => {
+//   for (let i = 0; i <= 9; i++) {
+//     axios.get(apiUri).then((response) => {
+//       console.log(response.data.response);
+//     });
+for (let i = 0; i <= 9; i++) {
+  axios.get(apiUri).then((response) => {
+    randomListEl.innerHTML += `
+        <li class="list-group-item">${response.data.response}</li>
+        `;
+  });
+}
